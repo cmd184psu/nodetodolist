a47e2536e1b76b2a9090806b3aa6a17fbc8f4511
CD on todo VM <cmd184psu@gmail.com>
2020-12-20 02:50:08 -0500

    adding filestojson.sh

---
+++ filestojson.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# usage: ./filestojson.sh > jpg.json
+
+find . -iname "*.jpg" | grep -iv recycle | sort > filelisting.txt
+
+LEN=`cat filelisting.txt | wc -l`
+
+i=0
+echo "["
+while IFS= read -r line; do
+    
+    line=${line:2}
+    echo "\"$line\""
+
+    if [ $i -lt `expr $LEN - 1` ]; then   
+        echo ","
+    fi    
+    #echo "$i :: $line"
+
+
+
+#    if [ "$i" == "0" ]; then
+#       echo "terminate early, just testing"
+#        exit 0
+#    fi
+
+    i=`expr ${i} + 1`
+done < filelisting.txt
+
+echo "]"
